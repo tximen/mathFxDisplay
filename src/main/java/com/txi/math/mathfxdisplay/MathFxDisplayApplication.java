9017eaac3addbc4d42f9518e22b8820c4f956815
Thomas Simon <thomas-h-simon@web.de>
2025-09-13 11:09:49 +0200

    spring boot

---
+++ src/main/java/com/txi/math/mathfxdisplay/MathFxDisplayApplication.java
@@ -0,0 +1,13 @@
+package com.txi.math.mathfxdisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathFxDisplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathFxDisplayApplication.class, args);
+	}
+
+}
